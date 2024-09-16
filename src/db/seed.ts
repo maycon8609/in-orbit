f8030da8682fec00e36fab43bdcb6846d7c35404
maycon silva <maycon8609@gmail.com>
2024-09-16 17:09:04 -0300

    refactor: update seed file

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,22 +1,43 @@
-import dayjs from "dayjs"
-import { client, db } from "."
-import { goalCompletions, goals } from "./schema"
+import dayjs from 'dayjs'
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
-  const result = await db.insert(goals).values([
-    { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-    { title: 'Me exercitar', desiredWeeklyFrequency: 4 },
-    { title: 'Estudar programação', desiredWeeklyFrequency: 5 }
-  ]).returning()
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Go to the gym', desiredWeeklyFrequency: 5 },
+      { title: 'Sleep early', desiredWeeklyFrequency: 6 },
+      { title: 'To study', desiredWeeklyFrequency: 6 },
+    ])
+    .returning()
 
   const startOfWeek = dayjs().startOf('week')
 
   await db.insert(goalCompletions).values([
-    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
-    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+    {
+      goalId: result[0].id,
+      createdAt: startOfWeek.add(1, 'day').add(8, 'hours').toDate(),
+    },
+    {
+      goalId: result[1].id,
+      createdAt: startOfWeek.add(1, 'day').add(20, 'hours').toDate(),
+    },
+    {
+      goalId: result[2].id,
+      createdAt: startOfWeek.add(1, 'day').add(9, 'hour').toDate(),
+    },
+    {
+      goalId: result[0].id,
+      createdAt: startOfWeek.add(2, 'day').add(8, 'hours').toDate(),
+    },
+    {
+      goalId: result[2].id,
+      createdAt: startOfWeek.add(2, 'day').add(9, 'hour').toDate(),
+    },
   ])
 }
 
